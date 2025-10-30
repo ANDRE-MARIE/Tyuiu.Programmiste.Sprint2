9275532407f37ffd91c4d3b474bb2e0222250de3
GNOLA25-1 <agdmdot@gmail.com>
2025-10-30 17:37:11 +0500

    Ajoutez des fichiers projet.

---
+++ Tyuiu.Programmiste.Sprint2.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.Programmiste.Sprint2.Task0.V15.Lib
+{
+    public class DataService : ISprint2Task0V15
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] result = new bool[6];
+            result[0] = x == y;
+            result[1] = x != x;
+            result[2] = y < x;
+            result[3] = x > y;
+            result[4] = x<= y;
+            result[5] = y >= x;
+            return result;
+
+        }
+    }
+}
