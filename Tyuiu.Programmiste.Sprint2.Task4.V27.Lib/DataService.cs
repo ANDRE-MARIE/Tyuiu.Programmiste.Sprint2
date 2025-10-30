aa486a2cbb63236c32923ac539d6b86ad9dac363
GNOLA25-1 <agdmdot@gmail.com>
2025-10-30 19:27:37 +0500

    Task4

--- Tyuiu.Programmiste.Sprint2.Task4.V27.Lib/DataService.cs
+++ Tyuiu.Programmiste.Sprint2.Task4.V27.Lib/DataService.cs
@@ -7,12 +7,24 @@ namespace Tyuiu.Programmiste.Sprint2.Task4.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            double result = (x * 10 > y + 2)
-                ? x * Math.Pow((y + 2) / (x - 1), x)
-                : Math.Pow(y, 2) - (2 / x);
+            if (x * 10 > y + 2)
+            {
+                if (Math.Abs(x - 1) < 0.0001) // Éviter division par zéro
+                    return 0;
 
-            return Math.Round(result, 3);
+                return Math.Round(x * Math.Pow((y + 2) / (x - 1), x), 3);
+            }
+            else
+            {
+                if (Math.Abs(x) < 0.0001) // Éviter division par zéro
+                    return 0;
+
+                return Math.Round(Math.Pow(y, 2) - (2 / x), 3);
+            }
         }
     }
 }
-    
+
+
+
+
